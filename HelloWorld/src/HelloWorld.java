1835bacca166b5eea75004bfcb0b6dbdc483543a
DeividSfrink <deividsfrink@hotmail.com>
2020-03-28 16:48:43 +0100

    First commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+
+        /*
+        What's the meaning of System.out.println in Java?
+        -------------------------------------------------
+        System.out.println is a Java statement that prints the argument passed, into the System.out which is generally stdout.
+        It can be 3 types:
+        - Standard input(stdin) - this is the file handle that your process reads to get information from you.
+        - Standard output(stdout) - your process writes normal information to this file handle.
+        - Standard error(stderr) - your process writes error information to this file handle.
+        */
+    }
+}
+
